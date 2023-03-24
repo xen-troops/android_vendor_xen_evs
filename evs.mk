57aa50fea07e7ad1cc4a5ec67e17d9250e26eddd
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2023-03-24 09:38:30 +0200

    Set correct SELinux context
    Set correct SELinux context to
    custom sampleDriver implementation.
    
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

--- evs.mk
+++ evs.mk
@@ -2,3 +2,5 @@ PRODUCT_PACKAGES += android.hardware.automotive.evs@1.1-xt
 
 PRODUCT_COPY_FILES += \
     vendor/xen/evs/config_override.json:/system/etc/automotive/evs/config_override.json \
+
+include vendor/xen/evs/sampleDriver/sepolicy/evsdriver.mk
